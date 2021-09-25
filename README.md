60e06837f2320a4aaebfcba9ef2ebaac2e6be744
henriqueTsugiyama <henrique-tsu@hotmail.com>
2021-09-25 13:05:55 -0300

    initialilizing react environment

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # route-for-you-frontend
 Distribution system interface
+
+## To get started
+yarn install or yarn
+
+## Run project
+yarn start
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.css';
+import {Home} from './pages/'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
